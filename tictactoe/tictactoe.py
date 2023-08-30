82e5c1eb5ed8844c15428c8a6fd7513636b2b066
Colin McKinney <42683765+cbpmckinney@users.noreply.github.com>
2023-08-29 20:40:43 -0400

    Updated 29 August from class

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -2,13 +2,41 @@
 # Uses PyGame Community Edition
 # And Minimax/Alpha-beta pruning
 
+def CheckWin(currentboard):
+# function should return 0 if no winner but also no draw
+# return 1 if X wins
+# return 2 if O wins
+# return 3 if draw
+    return 0
+
+def PrintBoard(currentboard):
+    print(currentboard[0:3])
+    print(currentboard[3:6])
+    print(currentboard[6:9])
 
 
+gameboard = [0] * 9 
+playerturn = 1 #1 is player 1, 2 is player 2
+gamestate = 0 #0 for playing, 1 for 1 wins, 2 for 2 wins, 3 for draw
+turnsplayed = 0
 
-def CheckWin():
-
-    return 
-
+while(gamestate == 0):
+    PrintBoard(gameboard)
+    if(playerturn == 1):
+        # Get player's input
+        playermove = int(input("Choose a space 0 through 8: "))
+        while(gameboard[playermove] != 0):
+            playermove = input("Invalid move; choose again: ")
+        gameboard[playermove] = 1
+        playerturn = 2
+        turnsplayed = turnsplayed+1
+    else:
+        #get AI's input
+        aimove = minimax(gameboard, playerturn, turnsplayed)
+        gameboard[aimove] = 2
+        playerturn = 1
+        turnsplayed = turnsplayed+1
+    
 
 
 

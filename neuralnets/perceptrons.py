30e163fe4d3ce2aa5b453ca639080a46cb422a45
Colin McKinney <mckinnec@wabash.edu>
2023-09-21 11:01:55 -0400

    Updated gradient descent

--- neuralnets/perceptrons.py
+++ neuralnets/perceptrons.py
@@ -24,6 +24,37 @@ class Perceptron:
             print("Dimension mismatch!")
 
 
+    def error(self, outputlist, truelist):
+        sum = 0
+        n = len(outputlist)
+        m = len(outputlist[0])
+        for i in range(n):
+            for j in range(m):
+                sum += (outputlist[i][j]-truelist[i])**2
+        sum = sum/n
+        return sum
+    
+    def gradient(self, inputlist, outputlist, truelist):
+        n = len(inputlist)
+        m = len(inputlist[0])
+        partweights = np.zeros(m)
+        partbias = 0
+        for i in range(n):
+            error = self.bias + outputlist[i] - truelist[i]
+            partbias += error 
+            for j in range(m):
+                partweights[j] += error * inputlist[i][j]
+        partweights *= 2/n
+        partbias *= 2/n
+
+        return (partweights, partbias)
+
+    def updateweights(self, gradient, eta):
+        self.weights = self.weights - eta*gradient[0]
+        self.bias = self.bias - eta*gradient[1]
+            
+
+
 
 
 Spam = Perceptron(10,True,None)
@@ -31,15 +62,11 @@ Spam = Perceptron(10,True,None)
 print(Spam.weights)
 
 inputs1 = np.random.default_rng().random(10)
-inputs2 = np.random.default_rng().random(20)
+inputs2 = np.random.default_rng().random(10)
 
 output1 = Spam.evaluate(inputs1)
 output2 = Spam.evaluate(inputs2)
-
-print(output1)
-print(type(output1))
-print(output2)
-
+print(output1, output2)
 
 
 
